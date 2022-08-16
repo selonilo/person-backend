7f87c2bde109d744b40c35bed5d3ddb2d071292b
Salih Çetin <salihcetinteam@gmail.com>
2022-08-16 17:41:23 +0300

    Add Post Method

--- src/main/java/com/personlist/person/controller/PersonController.java
+++ src/main/java/com/personlist/person/controller/PersonController.java
@@ -1,7 +1,24 @@
 package com.personlist.person.controller;
 
+import com.personlist.person.model.dto.PersonSearch;
+import com.personlist.person.service.PersonService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
+
+    private final PersonService personService;
+
+    public PersonController(PersonService personService){
+        this.personService = personService;
+    }
+
+    @PostMapping("/public/person")
+    public ResponseEntity getWorkersWithSort(Pageable pageable, @RequestBody PersonSearch personSearch){
+        return ResponseEntity.ok(personService.findPersonWithPagination(pageable,personSearch));
+    }
 }
